6755ad06a28d3a77894002ebe8f457008732c0b0
PAKouzo <dohieuxk14@gmail.com>
2024-03-11 10:28:34 +0700

    expressJS

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # WEB77
 
-NodeJS
-Cách import module.
-
-- vào package.json tạo "type": "module"
-- export default + tên function.
-- import + tên function + from + địa chỉ.
-  Route:
-- dùng request.url;
+- Khi nào cần dùng params:
+  Chỉ nên dùng 1 params là id của rsc.
+- Khi nào dùng query:
+  Khi có nhiều tham số cần truyền vào, nhưng các tham số không cần bảo mật.
+  VD: tìm kiếm, sắp xếp, phân trang, lọc dữ liệu, ...
+- Khi nào dùng body:
+  Khi cần bảo mật thông tin.
+  Khi cần truyền nhiều tham số vào server.
+- Các phương thức:
+  read - get
+  create - post
+  update - put
+  delete - delete
+- filter -> trả về hàm.
+- find -> trả về object.
--- index.js
+++ index.js
@@ -1,19 +1,72 @@
-import http from 'http';
-import data from './data.js';
-const server = http.createServer((request, response) => {
-    const endpoint = request.url;
-    switch (endpoint) {
-        case '/':
-            response.end("Welcome!");
-            break;
-        case '/students':
-            response.end(JSON.stringify(data));
-            break;
-        default:
-            response.end("Not found!");
-            break;
+import express from "express";
+import {v4 as uuidv4, v4} from "uuid";
+import {users, posts} from "./data.js";
+const app = express();
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("welcome!")
+})
+
+app.get('/users/:id', (req, res) => {
+    const {age} = req.query;
+    const {id} = req.params;
+    console.log("id:", id)
+    console.log("age:", age)
+    const userRes = users.filter(user => user.age > +age);
+    res.send(userRes)
+})
+
+/*lấy danh sách user hơn 50 tuổi:
+- tạo api get user hơn 50 tuổi.
+- viết logic để lấy user hơn 50 tuổi.
+*/
+
+app.get('/users/old', (req, res) => {
+    const usersRes = users.filter(users => users.age > 50);
+    res.send({
+        status: 200,
+        message: 'Find old users successfully',
+        user: usersRes
+    });
+})
+
+//ex2:
+/*Viết API tạo user với các thông tin như trên users, với id là random (uuid), email là duy nhất, phải kiểm tra được trùng email khi tạo user
+- create -> dùng phương thức post.
+    1. Viết API tạo users.
+    2. Viết hàm tạo ID (random, sử dụng uuid).
+    3. Viết hàm nhập userName, email, age, avatar.
+    4. Check email xem có bị trùng nhau không. (sử dụng find)
+    5. Thêm thông tin user vào file data.
+*/
+//--------------------------------------------------------------
+app.post('/createUsers', (req, res) => {
+    const {userName, email, age, avatar} = req.body;
+    console.log(userName,',', email,',', age,',', avatar)
+    const newUser = {
+        id: v4(),
+        userName,
+        email,
+        age,
+        avatar
     }
-});
-server.listen(8000, ()=>{
-    console.log("server is running!")
+    const checkEmail = () => {
+        const n = users.length;
+        for( let i = 0; i < n; i++ )
+        {
+            if(newUser.email == users[i].email)
+            {
+                console.log("Email already in use!")
+            }
+            else{
+                users.push(newUser);
+                res.send(users);
+            }
+        }
+    }
+    res.send(checkEmail());
+})
+app.listen(8000, () => {
+    console.log("server is running!");
 })
